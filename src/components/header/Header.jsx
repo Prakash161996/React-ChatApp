bfba32f34e8100a48f28373c43daa9af41330d34
Prakash161996 <prakashchoudhary1696@gmail.com>
2022-12-23 11:43:17 +0530

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="main-nav">
+        <div className="logo">
+          <h2>
+            <span>R</span>eact
+            <span>C</span>hat
+          </h2>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
